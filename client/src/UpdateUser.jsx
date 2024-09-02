5fea32eb58a3205d56dc09524c9e2cde3f48d4af
Manjunath V Gajendaragad <manjuvg7026@gmail.com>
2024-09-03 03:01:00 +0530

    first commit

---
+++ client/src/UpdateUser.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+function UpdateUser() {
+  const { id } = useParams();
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [age, setAge] = useState();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3005/getUser/" + id)
+      .then((result) => {
+        console.log(result);
+        setName(result.data.name);
+        setEmail(result.data.email);
+        setAge(result.data.age);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const Update = (e) => {
+    e.preventDefault();
+    axios
+    .put("http://localhost:3005/updateUser/"+id, { name, email, age })
+    .then((result) => {
+      console.log(result);
+      navigate("/");
+    })
+    .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded p-3">
+        <form onSubmit={Update}>
+          <h2>update User</h2>
+          <div className="mb-2">
+            <label htmlFor="">Name</label>
+            <input
+              type="text"
+              placeholder="Enter your Name"
+              className="form-control"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </div>
+          <div className="mb-2">
+            <label htmlFor="">Email</label>
+            <input
+              type="email"
+              placeholder="Enter your Email"
+              className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+          </div>
+          <div className="mb-2">
+            <label htmlFor="">Age</label>
+            <input
+              type="text"
+              placeholder="Enter your Age"
+              className="form-control"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            ></input>
+          </div>
+          <button className="btn btn-success">Update</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default UpdateUser;
